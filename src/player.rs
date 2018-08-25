b203d5e85601b659696c60203fc1adf44d15905a
Sebastian Benjamin <sebastiancbenjamin@gmail.com>
2018-08-24 17:00:03 -0700

    Make player object respond to movement keys

--- src/player.rs
+++ src/player.rs
@@ -1,15 +1,15 @@
 #[derive(Debug)]
 pub struct Player {
-    pub x: u32,
-    pub y: u32
+    pub x: i32,
+    pub y: i32
 }
 
 impl Player {
-    pub fn new(x: u32, y: u32) -> Player {
-        Player {x, y}
+    pub fn new(x: i32, y: i32) -> Player {
+        return Player {x, y}
     }
 
-    pub fn set_pos(&mut self, x: u32, y: u32) {
+    pub fn set_pos(&mut self, x: i32, y: i32) {
         self.x = x;
         self.y = y;
     }
