226b98658d384594f9a804f39b55b73a88746313
Braam Beresford <braam7beresford@gmail.com>
2018-08-26 17:54:14 -0700

    I think this loks good

--- src/game/map/tile.rs
+++ src/game/map/tile.rs
@@ -1,36 +1,41 @@
 use tcod::console::*;
 
+
+
+pub trait Tile{
+    fn new(x: i32, y: i32) -> Self;
+
+    fn walkeable(&self)->bool{
+        return true;
+    }
+    fn see_through(&self)->bool{
+        return true;
+    }
+
+    fn draw(&self, window: &Root);
+}
+
+
 #[derive(Debug)]
 pub struct Wall{
     pub x: i32,
     pub y: i32
 }
 
-impl Wall{
-    pub fn new(x: i32, y: i32) -> Wall{
-        return Wall{x, y};
+impl Tile for Wall{
+    fn new(x: i32, y: i32) -> Wall{
+        Wall{x: x, y: y}
     }
-    pub fn draw(&self, mut window: &Root) {
+
+    fn walkeable(&self)->bool{
+        return false;
+    }
+
+    fn see_through(&self)->bool{
+        return false;
+    }
+    
+    fn draw(&self, mut window: &Root) {
         window.put_char(self.x, self.y, '#', BackgroundFlag::None);
-    }
-}
-
-pub trait BlockProperties{
-    fn walkeable(&self)->bool{
-        return true;
-    }
-    fn see_through(&self)->bool{
-        return true;
-    }
-}
-
-
-
-impl BlockProperties for Wall{
-    fn walkeable(&self)->bool{
-        return false;
-    }
-    fn see_through(&self)->bool{
-        return false;
     }
 } 
