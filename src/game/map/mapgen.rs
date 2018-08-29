6286fb46bbc629428834cbb9afedc38a73576813
Henry Peterson <peterhen@oregonstate.edu>
2018-08-29 14:16:50 -0700

    bsp gen WIP

--- src/game/map/mapgen.rs
+++ src/game/map/mapgen.rs
@@ -1,6 +1,9 @@
+extern crate rand;
 use game::map::tile::Tile;
 use game::map::tile::Wall;
 use tcod::bsp::*;
+use self::rand::{thread_rng, Rng};
+use std::cmp::{min, max};
 
 pub fn dummy_gen() -> Vec<Box<Tile>> {
     let mut ret: Vec<Box<Tile>> = Vec::new();
@@ -52,9 +55,36 @@ pub fn bsp_gen(recursion_levels:     i32,
                                       max_horizontal_ratio, 
                                       max_vertical_ratio);
     bsp.traverse(TraverseOrder::LevelOrder, |node| {
-        //if node.is_leaf() {
-            box_draw(&mut ret, node.x, node.y, node.w, node.h);
-        //}
+        let mut rng = thread_rng();
+        if node.is_leaf() {
+            let point1: (i32, i32) = (rng.gen_range(node.x + 1, node.x + node.w),rng.gen_range(node.y + 1, node.y + node.h));
+            let point2: (i32, i32) = (rng.gen_range(node.x + 1, node.x + node.w),rng.gen_range(node.y + 1, node.y + node.h));
+            
+            box_draw(&mut ret, point1.0, point1.1, point2.0, point2.1);
+            /*
+            let x = rng.gen_range(node.x + 1, node.x + node.w);
+            let y = rng.gen_range(node.y + 1, node.y + node.h);
+            let mut tempvec: Vec<Box<Tile>> = Vec::new();
+            let root: Box = Box::new(Wall::new(x, y));
+            tempvec.push(root);
+            let mut temp: Box = Box::new(Wall::new(-1, -1));
+            while *root.get_x() != *temp.get_x() || *root.get_y() != *temp.get_y() {
+                if !ret.contains(Box::new(::new(8temp.get_x()), *temp.get_y()) && !tempvec.contains(Tile::new(temp.get_x(), temp.get_y()) {
+                    println!("hello");
+                }
+            }
+            println!("{} {}", node.x, node.x + node.w);
+            let mut rng = thread_rng();
+            //let x1 =0;
+            //let x2 =5;
+            //let y1 =0;
+            //let y2 =5;
+            let x1 = rng.gen_range(node.x, ((node.x + node.w ) / 2) as i32);
+            let x2 = rng.gen_range(x1, node.x + node.w) + 1;
+            let y1 = rng.gen_range(node.y, ((node.y + node.h ) / 2) as i32);
+            box_draw(&mut ret, x1, y1, x2, y2);
+            */
+        }
         return true;
     });
     return ret;
